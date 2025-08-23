9e8e5c4078fce13224e868b0127ca2e5d1666667
gn5r <via.gn5r@hotmail.com>
2025-08-24 05:24:58 +0900

    feat: add createGetCommand to CLI for executing GET requests

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { version } from "../package.json";
+import { createGetCommand } from "./command/get";
 
 export function createCli() {
   return yargs(hideBin(process.argv))
     .scriptName("backlog")
     .usage("使い方: $0 <command> [args]")
+    .command(createGetCommand())
     .version(version)
     .alias("v", "version")
     .describe("version", "バージョン情報を表示します")
