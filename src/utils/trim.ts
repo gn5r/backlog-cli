4f2f9471a752e15a6ec5a5932317f12a3683af23
gn5r <via.gn5r@hotmail.com>
2025-08-24 19:24:06 +0900

    feat: refine trim function to enforce type constraints and improve return type

--- src/utils/trim.ts
+++ src/utils/trim.ts
@@ -1,7 +1,7 @@
 import type { ArgumentsCamelCase } from "yargs";
 
-export function trim<U = unknown>(argv: ArgumentsCamelCase<U>) {
+export function trim<U extends object>(argv: ArgumentsCamelCase<U>): Omit<U, "_" | "$0"> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { _, $0, ...args } = argv;
-  return args;
+  return args as Omit<U, "_" | "$0">;
 }
