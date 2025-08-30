d6f445cf726db963a336885d489e1d1d427e6e00
gn5r <via.gn5r@hotmail.com>
2025-08-31 02:36:15 +0900

    refactor: consolidate issueIdOrKey option definition for improved clarity

--- src/command/get/issues/issue.ts
+++ src/command/get/issues/issue.ts
@@ -1,8 +1,8 @@
 import { AbstractSubCommand } from "@/command/abstractSubCommand";
 import { createBacklog } from "@/client";
 import { trim } from "@/utils/trim";
+import { issueIdOrKeyOption, type IssueIdOrKeyOption } from "@/options/issue/issueIdOrKeyOption";
 
-import type { IssueIdOrKeyOption } from "@/options/issue/issueIdOrKeyOption";
 import type { ArgumentsCamelCase, CommandBuilder } from "yargs";
 
 export class GetIssue extends AbstractSubCommand<unknown, IssueIdOrKeyOption> {
@@ -10,9 +10,7 @@ export class GetIssue extends AbstractSubCommand<unknown, IssueIdOrKeyOption> {
   describe =
     "参加しているプロジェクトから課題の情報を取得します。\n課題は、課題一覧の取得から検索できます。\n自分が参加しているプロジェクトの一覧はプロジェクト一覧の取得から取得できます。\nhttps://developer.nulab.com/ja/docs/backlog/api/2/get-issue/";
   public builder: CommandBuilder<unknown, IssueIdOrKeyOption> = (yargs) =>
-    yargs
-      .options({ issueIdOrKey: { type: "string", description: "", demandOption: true } })
-      .parserConfiguration({ "camel-case-expansion": false });
+    yargs.options(issueIdOrKeyOption).parserConfiguration({ "camel-case-expansion": false });
   protected async execute(argv: ArgumentsCamelCase<IssueIdOrKeyOption>): Promise<void> {
     const { issueIdOrKey } = trim<IssueIdOrKeyOption>(argv);
     const client = createBacklog();
--- src/options/issue/issueIdOrKeyOption.ts
+++ src/options/issue/issueIdOrKeyOption.ts
@@ -1,3 +1,12 @@
 export type IssueIdOrKeyOption = {
   issueIdOrKey: string;
 };
+
+export const issueIdOrKeyOption = {
+  issueIdOrKey: {
+    type: "string",
+    description:
+      "以下のいずれかを指定します。\n・課題のID\n・課題キー\n課題のIDと課題キーは課題一覧の取得から取得できます。",
+    demandOption: true,
+  },
+} as const;
